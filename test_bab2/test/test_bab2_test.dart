6c0a7f304efee8ebdce06db381a6fc08f10af5e0
adimarsesa <adimarsesa@gmail.com>
2020-11-01 23:23:35 +0700

    upload pertama bab2

---
+++ test_bab2/test/test_bab2_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_bab2/test_bab2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ test_bab2ok/bin/test_bab2ok.dart
@@ -0,0 +1,4 @@
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_bab2ok.calculate()}!');
+}
