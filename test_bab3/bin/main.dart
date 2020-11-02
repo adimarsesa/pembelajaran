1b05d79189ea02b8836f523c6796393a2d3dac0a
adimarsesa <adimarsesa@gmail.com>
2020-11-02 22:59:39 +0700

    upload bab 3

---
+++ test_bab3/bin/main.dart
@@ -0,0 +1,9 @@
+import 'dart:io' ;
+
+main(List<String> arguments) {
+ int a,b;
+ a = 10;
+ b = a++;
+ print(a.toString() + "-" + b.toString() );
+  }
+
---
+++ test_bab3/test/test_bab3_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_bab3/test_bab3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
