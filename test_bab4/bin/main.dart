d3d611f36c312089db4ad84bb89a2a42a5b90dbf
adimarsesa <adimarsesa@gmail.com>
2020-11-06 14:25:05 +0700

    lanjutan bab4

--- test_bab4/bin/main.dart
+++ test_bab4/bin/main.dart
@@ -1,19 +1,36 @@
 import 'dart:io' ;
 
-double luas_segiempat(double panjang,double lebar){
-  double hasil;
-  hasil = panjang * lebar;
-  return hasil;
+// double luas_segiempat(double panjang,double lebar){
+//   double hasil;
+//   hasil = panjang * lebar;
+//   return hasil;
+// }
+
+// Stirng say (String from, String message )
+
+String say (String from, String message, {String to, String appName}){
+ return from + " say " + message + ((to != null) ? " to " + to : "") +
+     ((appName != null) ?
+     " dari " + appName : "");
 }
 
+ String say2 (String from, String message, [String to, String appName]){
+  return from + " say " + message + ((to != null) ? " to " + to : "") +
+      ((appName != null) ?
+      " dari " + appName : "");
+ }
+
+
+
 main(List<String> arguments) {
- double p , l , luas;
+ print(say("jonny", "hello", to: "doloris", appName: "Whatsapp" )) ;
+ // double p , l , luas;
 
- p = double.tryParse(stdin.readLineSync());
- l = double.tryParse(stdin.readLineSync());
-
- // luas = luas_segiempat(p, l);
- 
- print(luas_segiempat(p, l));
+ // p = double.tryParse(stdin.readLineSync());
+ // l = double.tryParse(stdin.readLineSync());
+ //
+ // // luas = luas_segiempat(p, l);
+ //
+ // print(luas_segiempat(p, l));
 
 }
